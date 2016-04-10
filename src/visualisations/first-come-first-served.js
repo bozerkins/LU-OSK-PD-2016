b58af32c409852f236ddea10eb01a57a391dc897
bogdan <b.ozerkins@gmail.com>
2016-04-10 15:32:55 +0300

    fix for fcfs and sjf

--- src/visualisations/first-come-first-served.js
+++ src/visualisations/first-come-first-served.js
@@ -1,83 +1,103 @@
 angular.module('project')
-.controller('FirstComeFirstServedController', ['$scope', function($scope) {
-    $scope.template = 'first-come-first-served';
+    .controller('FirstComeFirstServedController', ['$scope', function($scope) {
+        $scope.processesAutoIncrement = 0;
+        $scope.process = {
+            'arrival_time': 0,
+            'burst_time': 0
+        };
+        $scope.processes = [];
+        $scope.schedule = [];
 
-    $scope.processesAutoIncrement = 0;
-    $scope.process = {
-      'arrival_time': 0,
-      'burst_time': 0
-    };
-    $scope.processes = [];
-    $scope.schedule = [];
+        $scope.createProcess = function(formProcess) {
+            $scope.processesAutoIncrement++;
+            var process = {};
+            process.name = 'P' + $scope.processesAutoIncrement;
+            process.arrival_time = formProcess.arrival_time;
+            process.burst_time = formProcess.burst_time;
+            $scope.processes.push(process);
+            $scope.processes.sort(function(a,b) {
+                return a.arrival_time > b.arrival_time ? 1 : -1;
+            });
 
-    $scope.createProcess = function(formProcess) {
-        $scope.processesAutoIncrement++;
-        var process = {};
-        process.name = 'P' + $scope.processesAutoIncrement;
-        process.arrival_time = formProcess.arrival_time;
-        process.burst_time = formProcess.burst_time;
-        $scope.processes.push(process);
-        $scope.createSchedule(process, $scope.processes.length - 1);
-        $scope.resetProcess();
-    };
-    $scope.createSchedule = function(process, key) {
-      if (!$scope.schedule.length) {
-        $scope.schedule.push({
-          'name': process.name,
-          'arrival_time' : process.arrival_time,
-          'start_time' : 0,
-          'end_time' : process.burst_time
-        });
-      } else {
-        var lastSchedule = $scope.schedule[$scope.schedule.length - 1];
-        $scope.schedule.push({
-          'name': process.name,
-          'arrival_time' : process.arrival_time,
-          'start_time' : lastSchedule.end_time,
-          'end_time' : lastSchedule.end_time + process.burst_time
-        });
-      }
-      $scope.updateSeconds();
-    };
-    $scope.resetProcess = function() {
-        $scope.process.arrival_time = 0;
-        $scope.process.burst_time = 0;
-    };
-    $scope.dropProcesses = function() {
-      // while not empty process list
-      while($scope.processes.length) {
-        // remove process
-        $scope.processes.pop();
-      }
-      // while not empty process list
-      while($scope.schedule.length) {
-        // remove process
-        $scope.schedule.pop();
-      }
-      // reset autoincrement
-      $scope.processesAutoIncrement = 0;
-      // reset total scheduled seconds
-      $scope.updateSeconds();
-    };
+            //$scope.createSchedule(process, $scope.processes.length - 1);
+            $scope.generateSchedule();
+            $scope.resetProcess();
+        };
+        $scope.generateSchedule = function() {
+            // reset what's done
+            $scope.dropSchedule();
 
-    $scope.fillProcessesWithTestData = function() {
-      for(var key = 0; key < 5; key++) {
-        $scope.createProcess({
-          arrival_time: Math.floor((Math.random() * 10) + 1),
-          burst_time: Math.floor((Math.random() * 10) + 1)
-        });
+            // generate a new one
+            for(var key in $scope.processes) {
+                var process = $scope.processes[key];
+                if (!$scope.schedule.length) {
+                    $scope.schedule.push({
+                        'name': process.name,
+                        'arrival_time' : process.arrival_time,
+                        'start_time' : process.arrival_time,
+                        'end_time' : process.arrival_time + process.burst_time
+                    });
+                } else {
+                    var lastSchedule = $scope.schedule[$scope.schedule.length - 1];
+                    var startTime = process.arrival_time < lastSchedule.end_time ? lastSchedule.end_time : process.arrival_time;
+                    $scope.schedule.push({
+                        'name': process.name,
+                        'arrival_time' : process.arrival_time,
+                        'start_time' : startTime,
+                        'end_time' : startTime + process.burst_time
+                    });
+                }
+            }
+            // reset total scheduled seconds
+            $scope.updateSeconds();
+        };
 
-      }
-    };
+        $scope.resetProcess = function() {
+            $scope.process.arrival_time = 0;
+            $scope.process.burst_time = 0;
+        };
 
-    $scope.updateSeconds = function() {
-      if (!$scope.schedule.length) {
-        $scope.seconds = 0;
-        $scope.secondsRange = [];
-      } else {
-        $scope.seconds = $scope.schedule[$scope.schedule.length - 1].end_time;
-        $scope.secondsRange = _.range(0,$scope.seconds + 1);
-      }
-    };
+        $scope.dropProcesses = function() {
+            // while not empty process list
+            while($scope.processes.length) {
+                // remove process
+                $scope.processes.pop();
+            }
+            // reset autoincrement
+            $scope.processesAutoIncrement = 0;
 
-}]);
+            $scope.dropSchedule();
+        };
+
+        $scope.dropSchedule = function() {
+            // while not empty process list
+            while($scope.schedule.length) {
+                // remove process
+                $scope.schedule.pop();
+            }
+            // reset total scheduled seconds
+            $scope.updateSeconds();
+        };
+
+
+        $scope.fillProcessesWithTestData = function() {
+            for(var key = 0; key < 5; key++) {
+                $scope.createProcess({
+                    arrival_time: Math.floor((Math.random() * 10) + 1),
+                    burst_time: Math.floor((Math.random() * 5) + 1)
+                });
+
+            }
+        };
+
+        $scope.updateSeconds = function() {
+            if (!$scope.schedule.length) {
+                $scope.seconds = 0;
+                $scope.secondsRange = [];
+            } else {
+                $scope.seconds = $scope.schedule[$scope.schedule.length - 1].end_time;
+                $scope.secondsRange = _.range(0,$scope.seconds + 1);
+            }
+        };
+
+    }]);
--- src/visualisations/shortest-job-first.js
+++ src/visualisations/shortest-job-first.js
@@ -1,4 +1,136 @@
 angular.module('project')
 .controller('ShortestJobFirstController', ['$scope', function($scope) {
-    $scope.template = 'shortest-job-first';
+    $scope.processesAutoIncrement = 0;
+    $scope.process = {
+        'arrival_time': 0,
+        'burst_time': 0
+    };
+    $scope.processes = [];
+    $scope.schedule = [];
+
+    $scope.createProcess = function(formProcess) {
+        $scope.processesAutoIncrement++;
+        var process = {};
+        process.name = 'P' + $scope.processesAutoIncrement;
+        process.arrival_time = formProcess.arrival_time;
+        process.burst_time = formProcess.burst_time;
+        $scope.processes.push(process);
+
+        $scope.generateSchedule();
+        $scope.resetProcess();
+    };
+    $scope.generateSchedule = function() {
+        // reset what's done
+        $scope.dropSchedule();
+
+        // store all process keys into array
+        var processesCache = _.map($scope.processes, function(value, key) {
+            return key.toString();
+        });
+
+        // choose schedule
+        while(processesCache.length) {
+
+            var currentStartTime = 0;
+            if ($scope.schedule.length) {
+                currentStartTime = $scope.schedule[$scope.schedule.length - 1].end_time;
+            }
+
+            var minExpectedEndTime = null;
+            var minExpectedEndTimeProcessKey = null;
+
+            for(var key in $scope.processes) {
+                // skip the process if not in processesCache
+                if (processesCache.indexOf(key) === -1) {
+                    continue;
+                }
+
+                var process = $scope.processes[key];
+
+                // evaluate the process
+                var expectedEndTime = currentStartTime < process.arrival_time
+                    ? (process.arrival_time + process.burst_time)
+                    : (currentStartTime + process.burst_time);
+
+                // check if min expected time
+                if (minExpectedEndTime === null || minExpectedEndTime > expectedEndTime) {
+                    minExpectedEndTime = expectedEndTime;
+                    minExpectedEndTimeProcessKey = key;
+                }
+            }
+
+            processesCache.splice(processesCache.indexOf(minExpectedEndTimeProcessKey), 1);
+
+            var process = $scope.processes[minExpectedEndTimeProcessKey];
+
+            if (!$scope.schedule.length) {
+                $scope.schedule.push({
+                    'name': process.name,
+                    'arrival_time' : process.arrival_time,
+                    'start_time' : process.arrival_time,
+                    'end_time' : process.arrival_time + process.burst_time
+                });
+            } else {
+                var lastSchedule = $scope.schedule[$scope.schedule.length - 1];
+                var startTime = process.arrival_time < lastSchedule.end_time ? lastSchedule.end_time : process.arrival_time;
+                $scope.schedule.push({
+                    'name': process.name,
+                    'arrival_time' : process.arrival_time,
+                    'start_time' : startTime,
+                    'end_time' : startTime + process.burst_time
+                });
+            }
+        }
+
+        // reset total scheduled seconds
+        $scope.updateSeconds();
+    };
+
+    $scope.resetProcess = function() {
+        $scope.process.arrival_time = 0;
+        $scope.process.burst_time = 0;
+    };
+
+    $scope.dropProcesses = function() {
+        // while not empty process list
+        while($scope.processes.length) {
+            // remove process
+            $scope.processes.pop();
+        }
+        // reset autoincrement
+        $scope.processesAutoIncrement = 0;
+
+        $scope.dropSchedule();
+    };
+
+    $scope.dropSchedule = function() {
+        // while not empty process list
+        while($scope.schedule.length) {
+            // remove process
+            $scope.schedule.pop();
+        }
+        // reset total scheduled seconds
+        $scope.updateSeconds();
+    };
+
+
+    $scope.fillProcessesWithTestData = function() {
+        for(var key = 0; key < 5; key++) {
+            $scope.createProcess({
+                arrival_time: Math.floor((Math.random() * 10) + 1),
+                burst_time: Math.floor((Math.random() * 5) + 1)
+            });
+
+        }
+    };
+
+    $scope.updateSeconds = function() {
+        if (!$scope.schedule.length) {
+            $scope.seconds = 0;
+            $scope.secondsRange = [];
+        } else {
+            $scope.seconds = $scope.schedule[$scope.schedule.length - 1].end_time;
+            $scope.secondsRange = _.range(0,$scope.seconds + 1);
+        }
+    };
 }]);
